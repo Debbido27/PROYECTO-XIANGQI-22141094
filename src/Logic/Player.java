2d29d2d3dea4f3d0c7a7aa073891918aa0ca084c
Debbido27 <davidmedina272004@gmail.com>
2026-04-27 15:22:19 -0600

    creacion de constructor

--- src/Logic/Player.java
+++ src/Logic/Player.java
@@ -12,5 +12,13 @@ public class Player {
     private int CuentaLog;
     
     
+    public Player(String username, String password){
+        this.username=username;
+        this.password=password;
+        this.puntos=0;
+        this.logs= new String [10];
+        this.CuentaLog=0;
+    }
+    
     
 }
