20de01a2428bd3d3947824c2756feb45cd33a085
Debbido27 <davidmedina272004@gmail.com>
2026-04-27 15:21:06 -0600

    Creacion de atributos

--- src/Logic/Player.java
+++ src/Logic/Player.java
@@ -1,13 +1,16 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package Logic;
 
-/**
- *
- * @author Dell
- */
+
 public class Player {
+    //Atributos
+    
+    String username;
+    String password;
+    private int puntos;
+    private String [] logs;
+    private int CuentaLog;
+    
+    
     
 }
