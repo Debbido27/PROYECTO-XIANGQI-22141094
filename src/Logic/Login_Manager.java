ab5b6b1a8fc58998bba3efb883700a1b3f7a289e
Debbido27 <davidmedina272004@gmail.com>
2026-04-27 17:09:35 -0600

    Se crearon lso atributos para guarda jugadores, y tambien el constructor

--- src/Logic/Login_Manager.java
+++ src/Logic/Login_Manager.java
@@ -2,6 +2,23 @@
 package Logic;
 
 public class Login_Manager {
+   
+    //Statica para no declararse en cada metodo con capacidad para 50 jugadores
+    private static final int MAX_PLAYERS=50;
+    
+    //Arreglos para la clase player para guarda jugadores
+    private Player CurrentUser;
+    private int totalPlayers;
+    private Player [] players;
+    
+    
+    public Login_Manager(){
+        
+        players = new Player[MAX_PLAYERS];
+        totalPlayers=0;
+        CurrentUser=null;
+    }
+    
     
     
     
