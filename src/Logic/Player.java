7984cd1ac78c59ab629164d02e478a3cea621566
Debbido27 <davidmedina272004@gmail.com>
2026-04-27 15:30:56 -0600

    Getter y setter de atributos

--- src/Logic/Player.java
+++ src/Logic/Player.java
@@ -19,6 +19,48 @@ public class Player {
         this.logs= new String [10];
         this.CuentaLog=0;
     }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getPuntos() {
+        return puntos;
+    }
+
+    public void setPuntos(int puntos) {
+        this.puntos = puntos;
+    }
+
+    public String[] getLogs() {
+        return logs;
+    }
+
+    public void setLogs(String[] logs) {
+        this.logs = logs;
+    }
+
+    public int getCuentaLog() {
+        return CuentaLog;
+    }
+
+    public void setCuentaLog(int CuentaLog) {
+        this.CuentaLog = CuentaLog;
+    }
+    
+    
     
     
 }
