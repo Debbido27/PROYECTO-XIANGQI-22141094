ca78af1143f5793c38a64bc8f2c519b9fa52151d
Debbido27 <davidmedina272004@gmail.com>
2026-04-27 21:12:50 -0600

    Ranking para los jugadores

--- src/Logic/Login_Manager.java
+++ src/Logic/Login_Manager.java
@@ -111,6 +111,26 @@ public class Login_Manager {
 
          
          
+            
+            public Player [] getRankingJugadores(){
+           Player[] ranking = new Player[totalPlayers];
+            for (int i = 0; i < totalPlayers; i++) {
+                ranking[i] = players[i];
+            }
+
+            //burbuja
+            for (int i = 0; i < totalPlayers - 1; i++) {
+                for (int j = 0; j < totalPlayers - 1 - i; j++) {
+                    if (ranking[j].getPuntos() < ranking[j + 1].getPuntos()) {
+                        Player temp = ranking[j];
+                        ranking[j] = ranking[j + 1];
+                        ranking[j + 1] = temp;
+                    }
+                }
+            }
+            return ranking;
+           }
+
          
          
          
