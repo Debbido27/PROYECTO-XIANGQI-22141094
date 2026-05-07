9a8b860e76e8fff1ad97bdcc6dc6d5596563b219
Debbido27 <davidmedina272004@gmail.com>
2026-05-07 17:32:40 -0600

    Getter y setter

--- src/Logic/Pieza.java
+++ src/Logic/Pieza.java
@@ -22,7 +22,33 @@ public abstract class Pieza {
     
     public abstract String getSimbolo();
     
+    //Metodos concretos
+
+    public int getFila() {
+        return fila;
+    }
+
+    public void setFila(int fila) {
+        this.fila = fila;
+    }
+
+    public int getColumna() {
+        return columna;
+    }
+
+    public void setColumna(int columna) {
+        this.columna = columna;
+    }
+
+    public boolean isIsR() {
+        return isR;
+    }
+
+    public void setIsR(boolean isR) {
+        this.isR = isR;
+    }
     
+   
     
     
     
