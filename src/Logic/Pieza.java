c3acbba960b6481f2911deee6c392fcf7d3191a2
Debbido27 <davidmedina272004@gmail.com>
2026-05-07 17:30:48 -0600

    Creacion de atributos y constructor

--- src/Logic/Pieza.java
+++ src/Logic/Pieza.java
@@ -6,5 +6,19 @@ package Logic;
  * @author Dell
  */
 public abstract class Pieza {
+    protected int fila;
+    protected int columna;
+    protected boolean isR;
+    
+    public Pieza(int fila, int columna, boolean isR){
+        this.fila=fila;
+        this.columna=columna;
+        this.isR=isR;
+    }
+    
+    
+    
+    
+    
     
 }
