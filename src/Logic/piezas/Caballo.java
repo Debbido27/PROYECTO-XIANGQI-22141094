e49f2a48f00622ebdad04130175fa0a00c586d08
Debbido27 <davidmedina272004@gmail.com>
2026-05-07 21:05:28 -0600

    lista de movimientos validos para el caballo

--- src/Logic/piezas/Caballo.java
+++ src/Logic/piezas/Caballo.java
@@ -18,5 +18,29 @@ public class Caballo extends Pieza {
     }
     
     
+    public boolean[][] getMoveValido(Pieza[][] tablero){
+        boolean[][] movimientos = new boolean [10][9];
+        
+       int[] dFila  = {-2, -2, -1, -1,  1,  1,  2,  2};
+        int[] dCol   = {-1,  1, -2,  2, -2,  2, -1,  1};
+        int[] blkFila = {-1, -1,  0,  0,  0,  0,  1,  1};
+        int[] blkCol  = { 0,  0, -1,  1, -1,  1,  0,  0};
+
+        for (int i = 0; i < 8; i++) {
+            int nf   = fila    + dFila[i];
+            int nc   = columna + dCol[i];
+            int bf   = fila    + blkFila[i];
+            int bc   = columna + blkCol[i];
+
+            if (enTablero(nf, nc) 
+                && tablero[bf][bc] == null 
+                && puedeMoverA(nf, nc, tablero)) {
+                movimientos[nf][nc] = true;
+            }
+        }
+        return movimientos;
+    }
+        
+    }
     
 }
