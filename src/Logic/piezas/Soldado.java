10a997727134d5115711e06767ad61f0df3adf47
Debbido27 <davidmedina272004@gmail.com>
2026-05-07 21:24:57 -0600

    Constructor

--- src/Logic/piezas/Soldado.java
+++ src/Logic/piezas/Soldado.java
@@ -4,6 +4,10 @@ package Logic.piezas;
 
 public class Soldado extends Pieza {
   
+    public Soldado(int fila, int columna, boolean isR){
+        super(fila, columna, isR);
+    }
+    
     
     
 }
