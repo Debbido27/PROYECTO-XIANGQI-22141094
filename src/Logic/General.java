2dbe045a3c1abd7838d3a9b8d79639ed69317e1f
Debbido27 <davidmedina272004@gmail.com>
2026-05-07 18:52:24 -0600

    Consructor con super

--- src/Logic/General.java
+++ src/Logic/General.java
@@ -4,4 +4,12 @@ package Logic;
 
 public final class General extends Pieza {
     
+    public General (int fila, int columna, boolean isR){
+        super(fila,columna, isR);
+    }
+    
+    
+    
+    
+    
 }
