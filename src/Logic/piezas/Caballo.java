33f3f5fbe0038e27262b537b1d797e8976f66c2d
Debbido27 <davidmedina272004@gmail.com>
2026-05-07 21:01:09 -0600

    Metodo de get simobolo, para saber si es rojo o negro

--- src/Logic/piezas/Caballo.java
+++ src/Logic/piezas/Caballo.java
@@ -7,4 +7,16 @@ package Logic.piezas;
  */
 public class Caballo extends Pieza {
     
+    public Caballo(int fila, int columna, boolean isR){
+        super(fila, columan, isR);
+    }
+    
+    
+    @Override
+    public String getSimbolo(){
+        return isR ? "傌" : "馬";
+    }
+    
+    
+    
 }
