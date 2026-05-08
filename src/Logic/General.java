02e38bebcdb781960fba0c63f4fb1c9da7c3e305
Debbido27 <davidmedina272004@gmail.com>
2026-05-07 20:07:37 -0600

    Meotod para poder obtener las piezas

--- src/Logic/General.java
+++ src/Logic/General.java
@@ -8,7 +8,31 @@ public final class General extends Pieza {
         super(fila,columna, isR);
     }
     
+    @Override
+    public String getSimbolo(){
+        return isR ? "将" : "帅";
+    }
     
+    @Override
+    public boolean[][] getMovimientosValidos(Pieza[][] tablero){
+        boolean [][] movimientos = new boolean[10][9];
+        
+        int[] dFila={-1,1,0,0};
+        int[] dCol ={0,0,-1,1};
+        
+        for (int i = 0; i < 4; i++) {
+            int nf = fila    + dFila[i];
+            int nc = columna + dCol[i];
+
+            if (enPalacio(nf, nc) && puedeMoverA(nf, nc, tablero)) {
+                movimientos[nf][nc] = true;
+            }
+        }
+        return movimientos;
+            
+            
+        }
+    }
     
     
     
