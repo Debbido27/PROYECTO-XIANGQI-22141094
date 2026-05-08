79f1cea8ecd9da0b553fb9e0576438b458439dd2
Debbido27 <davidmedina272004@gmail.com>
2026-05-07 21:07:59 -0600

    Constructor creado

--- src/Logic/piezas/Carro.java
+++ src/Logic/piezas/Carro.java
@@ -7,5 +7,10 @@ package Logic.piezas;
  */
 public class Carro extends Pieza{
     
+    public Carro(int fila, int columna, boolean isR){
+        super(fila,columna,isR);
+    }
+    
+    
     
 }
