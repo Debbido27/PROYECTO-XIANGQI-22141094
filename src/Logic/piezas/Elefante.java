d9664ebb5f6ab9c492923b48011175f9cbe6432a
Debbido27 <davidmedina272004@gmail.com>
2026-05-07 20:57:07 -0600

    Movimientos validos para el elefefante en move valido

--- src/Logic/piezas/Elefante.java
+++ src/Logic/piezas/Elefante.java
@@ -11,6 +11,29 @@ public class Elefante extends Pieza{
         super(fila,columna,isR);
     }
     
-    
+   
+    @Override
+    public boolean[][] getMoveValido(Pieza[][] tablero){
+        boolean[][] movimientos = new boolean[10][9];
+        
+        int[] dFila = {-2, -2,  2,  2};
+        int[] dCol  = {-2,  2, -2,  2};
+        int[] ojof  = {-1, -1,  1,  1};
+        int[] ojoc  = {-1,  1, -1,  1};
+
+        for (int i = 0; i < 4; i++) {
+            int nf   = fila    + dFila[i];
+            int nc   = columna + dCol[i];
+            int eyef = fila    + ojof[i];
+            int eyec = columna + ojoc[i];
+
+            if (enTablero(nf, nc) && !cruzaRio(nf) 
+                && tablero[eyef][eyec] == null 
+                && puedeMoverA(nf, nc, tablero)) {
+                movimientos[nf][nc] = true;
+            }
+        }
+        return movimientos;
+    }
     
 }
